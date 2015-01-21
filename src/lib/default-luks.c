5ee095a3a59e86615eb5748708155ab26bea37bc
Juergen Pabel <juergen@pabel.net>
2015-01-21 01:56:20 +0100

    Release 1.0.2

--- src/lib/default-luks.c
+++ src/lib/default-luks.c
@@ -6,8 +6,9 @@
 
 __attribute__ ((visibility ("hidden")))
 int default__api__luks_load(char* key, size_t* key_size) {
-	char	filename[FILENAME_MAX+1] = { 0 };
-	size_t	filename_size = sizeof(filename);
+	tt_status_t	status;
+	char		filename[FILENAME_MAX+1] = { 0 };
+	size_t		filename_size = sizeof(filename);
 
 	TT_TRACE( "plugin/default", "%s(key='%p',key_size=%p)", __FUNCTION__, key, key_size );
 	if( key == NULL || key_size == NULL || *key_size == 0 ) {
@@ -20,6 +21,13 @@ int default__api__luks_load(char* key, size_t* key_size) {
 	}
 	if( filename_size == 0 ) {
 		TT_LOG_ERROR( "plugin/default", "no file configured for LUKS key (luks|file)" );
+		return TT_IGN;
+	}
+	if( default__posix_exists( filename, &status ) != TT_OK ) {
+		TT_LOG_ERROR( "plugin/default", "default__posix_exists() failed for file '%s'", filename );
+		return TT_ERR;
+	}
+	if( status == TT_NO ) {
 		return TT_IGN;
 	}
 	if( default__posix_load( filename, (char*)key, key_size ) != TT_OK ) {
