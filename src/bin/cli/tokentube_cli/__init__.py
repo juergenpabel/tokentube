e8a6af3d1f7bf4e3c019e9a6366a1320f407311a
Juergen Pabel <juergen@pabel.net>
2015-03-06 01:14:05 +0100

    dynamic module loading for tokentube cli

--- src/bin/cli/tokentube_cli/__init__.py
+++ src/bin/cli/tokentube_cli/__init__.py
@@ -1,3 +1,16 @@
-from . import user
-from . import otp
+import sys, importlib, pkgutil
+
+
+def init(parser):
+	modules = loadAllModules()
+	for module in modules.values():
+		module.init(parser)
+
+
+def loadAllModules():
+	package = sys.modules[__name__]
+	return {
+		name: importlib.import_module(__name__ + '.' + name)
+		for loader, name, is_pkg in pkgutil.walk_packages(package.__path__)
+	}
 
