5cf6a81292f1e0b4c3a6273cd6b38926c6aa1a72
Juergen Pabel <juergen@pabel.net>
2015-03-17 21:54:03 +0100

    changed log component from 'library/init' to 'library/core'

--- src/lib/library.c
+++ src/lib/library.c
@@ -66,6 +66,13 @@ int libtokentube_library_finalize() {
 
 
 int tt_discover(tt_library_t* library) {
+	if( library == NULL ) {
+		TT_LOG_ERROR( "library/core", "invalid parameter in %s()", __FUNCTION__ );
+		return TT_ERR;
+	}
+	if( library->version.major == 0 && library->version.minor == 0 && library->version.patch == 0 ) {
+		library->version = TT_VERSION;
+	}
 	memcpy( &library->api, &g_library_api, sizeof(g_library_api) );
 	return TT_OK;
 }
